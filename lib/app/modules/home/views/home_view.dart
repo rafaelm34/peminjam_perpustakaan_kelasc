5369f57f473763b914af983ac4078016dd5298c9
Rafael <rafaelmaulana7666@gmail.com>
2024-01-31 15:06:06 +0700

    membuat project peminjam seperti project petugas

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:peminjam_perpustakaan_kelasc/app/modules/book/views/book_view.dart';
+import 'package:peminjam_perpustakaan_kelasc/app/modules/peminjaman/views/peminjaman_view.dart';
+import 'package:peminjam_perpustakaan_kelasc/app/routes/app_pages.dart';
 
 import '../controllers/home_controller.dart';
 
-class HomeView extends GetView<HomeController> {
+class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeView'),
-        centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => Get.toNamed(Routes.BOOK),
+              child: Text('Buku'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+            onPressed: () => Get.toNamed(Routes.PEMINJAMAN),
+              child: Text('Peminjaman'),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
